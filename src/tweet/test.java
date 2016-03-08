70f203d98e564148d2537ffec10752988391e558
kbowen99 <kurtisbowen99@gmail.com>
2016-03-08 09:11:55 -0600

    Actually Works

--- src/tweet/test.java
+++ src/tweet/test.java
@@ -1,25 +1,42 @@
 package tweet;
 
-import twitter4j.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 public class test {
 	
-	public test(){
-		System.out.println("Magic Commencing");
-	    // The factory instance is re-useable and thread safe.
-	    Twitter twitter = TwitterFactory.getSingleton();
-	    Query query = new Query("source:twitter4j yusukey");
-	    QueryResult result = null;
-		try {
-			result = twitter.search(query);
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			//System.exit(0);
+	ArrayList<Status> statuses = new ArrayList<>();
+	Twitter twitter = new TwitterFactory().getInstance();
+	String user;
+	
+	public test(String user){
+		this.user = user;
+		p("Magic Finding");
+		int pageno = 1;
+		boolean complete = false;
+		while (!complete) {
+		  try {
+		    int size = statuses.size(); 
+		    Paging page = new Paging(pageno++, 100);
+		    statuses.addAll(twitter.getUserTimeline(user, page));
+		    if (statuses.size() == size)
+		    	complete = true;
+		  }
+		  catch(TwitterException e) {
+		    e.printStackTrace();
+		  }
 		}
-	    for (Status status : result.getTweets()) {
-	        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-	    }
-	    System.out.println("Magic Successfull");
+		p("Magic Found");
 	}
+	
+	private void p(Object P){
+		System.out.println(P);
+	}
+	
 }
