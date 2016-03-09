0796db36fd04c66003c9a9ef30e008e2ccba6155
Hurro <kurtisbowen99@gmail.com>
2016-03-09 09:54:12 -0600

    Getting Tweets and posting tweets

--- src/tweet/Main.java
+++ src/tweet/Main.java
@@ -1,10 +1,13 @@
 package tweet;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test NoIdea = new test("billnye");
+		String creepinUser = JOptionPane.showInputDialog("Who We Creepin?");
+		test NoIdea = new test(creepinUser, false);
 	}
 
 }
--- src/tweet/test.java
+++ src/tweet/test.java
@@ -15,7 +15,7 @@ public class test {
 	Twitter twitter = new TwitterFactory().getInstance();
 	String user;
 	
-	public test(String user){
+	public test(String user, boolean limit){
 		this.user = user;
 		p("Magic Finding");
 		int pageno = 1;
@@ -25,14 +25,32 @@ public class test {
 		    int size = statuses.size(); 
 		    Paging page = new Paging(pageno++, 100);
 		    statuses.addAll(twitter.getUserTimeline(user, page));
-		    if (statuses.size() == size)
+		    if (statuses.size() == size || (statuses.size() >= 2000 && limit))
 		    	complete = true;
 		  }
 		  catch(TwitterException e) {
 		    e.printStackTrace();
 		  }
 		}
-		p("Magic Found");
+		p(statuses.size() + " Magics Found");
+		p("Spamming Now");
+		spamConsole();
+		p("Spam Complete");
+		String tmp = user + " First Tweet:" + statuses.get(statuses.size() - 1).getText();
+		sendTweet(((tmp.length() > 140) ? tmp.substring(0,140) : tmp));
+	}
+	
+	public void spamConsole(){
+		for (Status s : statuses)
+			p(s.getText());
+	}
+	
+	public void sendTweet(String s){
+		try {
+			twitter.updateStatus(s);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void p(Object P){
