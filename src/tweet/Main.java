a0c3f46ab68f43ed7dc235b414156a1fea8a8de5
Hurro <kurtisbowen99@gmail.com>
2016-03-10 09:22:03 -0600

    Clean up (sort of)

--- src/tweet/Main.java
+++ src/tweet/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String creepinUser = JOptionPane.showInputDialog("Who We Creepin?");
-		test NoIdea = new test(creepinUser, false);
+		test NoIdea = new test(creepinUser, true);
 	}
 
 }
--- src/tweet/test.java
+++ src/tweet/test.java
@@ -1,7 +1,7 @@
 package tweet;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import twitter4j.Paging;
 import twitter4j.Status;
@@ -12,27 +12,13 @@ import twitter4j.TwitterFactory;
 public class test {
 	
 	ArrayList<Status> statuses = new ArrayList<>();
+	HashMap<String, Integer> hmap = new HashMap<>();
 	Twitter twitter = new TwitterFactory().getInstance();
 	String user;
 	
-	public test(String user, boolean limit){
+	public test(String user){
 		this.user = user;
-		p("Magic Finding");
-		int pageno = 1;
-		boolean complete = false;
-		while (!complete) {
-		  try {
-		    int size = statuses.size(); 
-		    Paging page = new Paging(pageno++, 100);
-		    statuses.addAll(twitter.getUserTimeline(user, page));
-		    if (statuses.size() == size || (statuses.size() >= 2000 && limit))
-		    	complete = true;
-		  }
-		  catch(TwitterException e) {
-		    e.printStackTrace();
-		  }
-		}
-		p(statuses.size() + " Magics Found");
+		loadTweets();
 		p("Spamming Now");
 		spamConsole();
 		p("Spam Complete");
@@ -55,6 +41,25 @@ public class test {
 	
 	private void p(Object P){
 		System.out.println(P);
+	}
+	
+	private void loadTweets(){
+		p("Magic Finding");
+		int pageno = 1;
+		boolean complete = false;
+		while (!complete) {
+		  try {
+		    int size = statuses.size(); 
+		    Paging page = new Paging(pageno++, 100);
+		    statuses.addAll(twitter.getUserTimeline(user, page));
+		    if (statuses.size() == size || (statuses.size() >= 2000))
+		    	complete = true;
+		  }
+		  catch(TwitterException e) {
+		    e.printStackTrace();
+		  }
+		}
+		p(statuses.size() + " Magics Found");
 	}
 	
 }
